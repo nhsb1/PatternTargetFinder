8bf00ee8dcd87de9d9628403fde94feb25ae4472
nhsb1 <nhsb1@users.noreply.github.com>
2016-07-19 17:48:03 -0400

    featres - price and clipboard

--- ptf.py
+++ ptf.py
@@ -1,11 +1,20 @@
 from argparse import ArgumentParser 
+from yahoo_finance import Share
+import pyperclip
 
-currentrelease = 'Pattern Target Finder 1.0'
+currentrelease = 'Pattern Target Finder 1.1'
+#v1.1 - Get's current delayed price from Yahoo_Finance
+#		Allowed you to override price -p 123
+#		Copies output to the clipboard
+#v1.0 - calculates the pattern's target
+
 
 def getArgs():
     parser = ArgumentParser(description = currentrelease)
-    parser.add_argument("-rh", "--high", required=False, dest="high", help="recent high", metavar="high")
-    parser.add_argument("-rl", "--low", required=False, dest="low", help="recnet low", metavar="low")
+    parser.add_argument("-t", "--ticker", required=False, dest="ticker", help="ticker for lookup", metavar="ticker")
+    parser.add_argument("-p", "--price", required=False, dest="price", help="specify price", metavar="high")
+    parser.add_argument("-rh", "--high", required=True, dest="high", help="recent high", metavar="high")
+    parser.add_argument("-rl", "--low", required=True, dest="low", help="recent low", metavar="low")
     args = parser.parse_args()
     return args
 
@@ -17,10 +26,39 @@ def patterntarget(high, move):
 	target = float(high) + float(move)
 	return target
 
+def priceWork(stock):
+	"""
+    Returns delayed price for ticker and reporting for ticker from Yahoo_Finance module (Yahoo_finance module delayed price can be VERY delayed, hence realtime module)
+    """
+	subpricequote = stock.get_price()
+	#output.append(subpricequote)
+	return subpricequote
+
+#def clipboard1(ticker, price, move, target):
+
+
+
 #----------------------------------------------------------------------
 if __name__ == "__main__":
 	myargs = getArgs()
+	if myargs.ticker is not "" and myargs.price is None: #if you don't specify -p price then it get's the current delayed price
+		stock = Share(myargs.ticker)
+		price = priceWork(stock)
+	elif myargs.ticker is not "" and myargs.price > 0: #if you specify price, then it uses that as the starting price.
+		#print myargs.ticker, myargs.price
+		stock = myargs.ticker
+		price = myargs.price
 	move = patternmove(myargs.high, myargs.low)
 	target = patterntarget(myargs.high, move)
-	print target
+	#print target
+
+	if myargs.ticker is not "" and price is not None:
+		print myargs.ticker + ", " + str(price) + ", " + str(target)
+		newpasteitem = myargs.ticker + ", " + str(price) + ", " + str(target)
+		pyperclip.copy(newpasteitem)
+	else:
+		print target
+		newpasteitem = str(target)
+		pyperclip.copy(newpasteitem)
+
 	
